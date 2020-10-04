f709d19cff5db7411218f558e8c8f1a357a0fb6d
Seungwoo Wee <weechic0716@gmail.com>
2020-10-04 11:19:23 +0900

    test

--- parser_delete.py
+++ parser_delete.py
@@ -1,12 +1,22 @@
 import os
+import urllib
+import datetime
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
 import django
+from operator import itemgetter
 
 django.setup()
 
-from HotList.models import HotList
-from HoobangList.models import HoobangList
+from urllib.request import urlopen
+from bs4 import BeautifulSoup as BS
+import json
+import schedule
+import time
+import requests
+from elections.models import Candidate
+from hoobang.models import hoobang
+from datetime import date, timedelta
 
-HotList.objects.all().delete()
-HoobangList.objects.all().delete()
+Candidate.objects.all().delete()
+hoobang.objects.all().delete()
